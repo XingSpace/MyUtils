e1de40a309cc0f311654343f98a0b10030f35abc
Xing <366549434@qq.com>
2019-10-10 17:01:13 +0800

    添加日期时间处理工具类

---
+++ app/src/main/java/com/xing/app/myutils/Utils/DateUtil.java
@@ -0,0 +1,73 @@
+package com.xing.app.myutils.Utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 时间工具类
+ */
+public class DateUtil {
+
+    public static String yyyyMMdd = "yyyy-MM-dd";
+    public static String yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
+
+    public static long convert2Long(String date){
+        return convert2Long(date,yyyyMMddHHmmss);
+    }
+
+    /**
+     * 将字符串形式的时间，转换为long类型，适用于比较时间
+     * @return 以毫秒为单位的date时间
+     */
+    public static long convert2Long(String date,String format){
+        if (StringUtil.isEmpty(date,format)) return 0L;
+
+        try {
+            SimpleDateFormat sf = new SimpleDateFormat(format, Locale.getDefault());
+            return sf.parse(date).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 0L;
+    }
+
+    public static Date convert2Date(String date){
+        return convert2Date(date,yyyyMMddHHmmss);
+    }
+
+    public static Date convert2Date(String date,String format){
+        if (StringUtil.isEmpty(date,format)) return null;
+
+        try {
+            SimpleDateFormat sf = new SimpleDateFormat(format, Locale.getDefault());
+            return sf.parse(date);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String convert2String(long time){
+        return convert2String(time,yyyyMMddHHmmss);
+    }
+
+    /**
+     * 将毫秒形式的时间，转换为String
+     * @return 返回字符串格式的时间戳
+     */
+    public static String convert2String(long time,String format){
+        if (time <= 0 || StringUtil.isEmpty(format)) return "";
+        return convert2String(new Date(time),format);
+    }
+
+
+    public static String convert2String(Date date,String format){
+        SimpleDateFormat sf = new SimpleDateFormat(format, Locale.getDefault());
+        return sf.format(date);
+    }
+
+}
