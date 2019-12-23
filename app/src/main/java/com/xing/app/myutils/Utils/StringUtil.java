a6aa32c2c8fedd5548d769a64a35309388eda8bc
Xing <366549434@qq.com>
2019-12-23 21:14:46 +0800

    DrawableBuilder新增StateListDrawable创建方法

--- app/src/main/java/com/xing/app/myutils/Utils/StringUtil.java
+++ app/src/main/java/com/xing/app/myutils/Utils/StringUtil.java
@@ -3,7 +3,7 @@ package com.xing.app.myutils.Utils;
 public class StringUtil {
 
     public static boolean isEmpty(String s){
-        return s.equals("") || s.length() == 0;
+        return s == null || s.length() == 0;
     }
 
     /**
